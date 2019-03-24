937b9d676ea18289e6fa79daa3dcdf3a49e75b29
JoyAhmed <joy_ahmed_007@yahoo.com>
2019-03-25 00:07:42 +0600

    Add project files.

---
+++ SideNav/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SideNav.Models.AccountViewModels
+{
+    public class LoginWithRecoveryCodeViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Recovery Code")]
+        public string RecoveryCode { get; set; }
+    }
+}
